2fcada4bb4a54ebcca7bbc63a4a51dc4dfc5b4fc
Vadhiraj <65805544+vadhiraj-kv@users.noreply.github.com>
2025-04-23 21:28:44 +0530

    Project 6 completed

--- Project_6/project6.js
+++ Project_6/project6.js
@@ -40,20 +40,78 @@ function addTask() {
 }
 
 function deleteAllTask() {
-  
+  todo = [];
+  saveToLocalStorage();
+  displayTask();
+
 }
 
 function displayTask() {
   todoList.innerHTML = "";
-  todo.foreach((item,index) =>{
-    const p = document.createAttribute("p")
+  todo.forEach((item,index) =>{
+    const p = document.createElement("p")
     p.innerHTML = `
       <div class="todo-container">
-      <input type="checkbox" class="todo-checkbox" id="input-${index}">
+      <input type="checkbox" class="todo-checkbox" id="input-${index}" ${item.disabled ? 'checked' : '' }>
+
+      <p id="todo-${index}" class="${item.disabled ? "disabled" : ""}" onclick="editTask(${index})">${item.text}</p>
+      
       </div>
-    `
-  })
+    `;
+
+    p.querySelector(".todo-checkbox").addEventListener("change",() =>{
+      toggleTask(index)
+    });
+
+    todoList.appendChild(p)
+
+  });
+
+
+  todoCount.innerText = todo.length;
 }
+
+function editTask(index) {
+
+  const todoItem = document.getElementById(`todo-${index}`)
+  const existingText = todo[index].text;
+  const inputElement = document.createElement("input");
+
+
+  inputElement.value = existingText;
+  todoItem.replaceWith(inputElement)
+
+  inputElement.focus();
+
+  const updatedText = () => {
+    const updatedText = inputElement.value.trim();
+    if(updatedText){
+      todo[index].text = updatedText;
+      saveToLocalStorage()
+    }
+    displayTask()
+  }
+
+  inputElement.addEventListener("blur", updatedText)
+
+  inputElement.addEventListener("keydown",(e) => {
+    if(e.key === 'Enter'){
+      e.preventDefault();
+      updatedText();
+    }
+  })
+  
+}
+
+
+
+
+function toggleTask(index) {
+  todo[index].disabled = !todo[index].disabled;
+  saveToLocalStorage()
+  displayTask()
+}
+
 function saveToLocalStorage() {
   localStorage.setItem("todo",JSON.stringify(todo))
 }
